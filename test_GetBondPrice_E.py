1f82821d3358d84ca71bdec97455df47929d6837
David Romoff <DaveTheMovie@gmail.com>
2020-11-17 22:32:23 -0500

    Update test_GetBondPrice_E.py

--- test_GetBondPrice_E.py
+++ test_GetBondPrice_E.py
@@ -2,4 +2,8 @@
 import GetBondPrice_E
 
 def test_GetBondPriceE():
-    assert round(GetBondPrice_E.getBondPrice_E()) == 2098949 
+    yc = [.010,.015,.020,.025,.030]
+    face = 2000000
+    couponRate = .04
+    m = 10
+    assert round(GetBondPrice_E.getBondPrice_E(face, couponRate, m, yc)) == 2098949 
